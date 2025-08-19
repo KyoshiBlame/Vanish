f9e70b72792b8d1bec30d123da5c207157eb4a8a
kyoshi <give_me_gg@mail.ru>
2025-08-19 17:23:25 +0700

    vers2

--- cmd/main.go
+++ cmd/main.go
@@ -1,8 +1,9 @@
 package main
 
-import "archh/Vanish/service"
+import "Vanish/service"
 
 func main() {
 	pist := &service.Pistol{Ammo: 3}
 	service.Fire(pist)
+	//too
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Vanish
+
+go 1.24.6
